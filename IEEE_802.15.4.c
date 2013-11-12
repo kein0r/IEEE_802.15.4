a23082757129abe48c1b83393ad9c7aa6ef921ee
kein0r <github@dummgebabbel.de>
2013-11-12 07:53:48 +0000

    Added existing code to IEEE_802.15.4 files Added config file IEEE_802.15.4_cfg.h

--- IEEE_802.15.4.c
+++ IEEE_802.15.4.c
@@ -1,6 +1,13 @@
 /*******************| Inclusions |*************************************/
 #include "IEEE_802.15.4.h"
-//#include <IEEE_802.15.4_cfg.h>
+#include <IEEE_802.15.4_cfg.h>
+#include <ioCC2530.h>
+#include <board.h>
+
+/**
+ * Implementation only covers MAC layer since PHYS layer is completely handled by
+ * the MCU in hardware.
+*/
 
 /*******************| Macros |*****************************************/
 
@@ -9,6 +16,78 @@
 /*******************| Global variables |*******************************/
 
 /*******************| Function definition |****************************/
-void teste(void)
+
+/**
+ * ISR for radio Rx interrupt
+*/
+#pragma vector = RF_VECTOR
+__near_func __interrupt void IEE802154_radioRxISR(void)
 {
+  uint8 overallBufferLength;
+  uint8 payloadLength;
+  if( RFIRQF0 & IEEE802154_RFIRQF0_RXPKTDONE ) /* A complete frame has been received. */
+  {
+    /* get overall buffer length since it could be more than one frame in FIFO */
+    overallBufferLength = RXFIFOCNT;
+    /* handle receive interrupt, first read payload length from rx-buffer. */
+    payloadLength = RFD;      
+    
+    /* ACK frame ? */
+    if (payloadLength == RADIO_ACK_PACKET_SIZE)
+    {
+      
+    }
+    else /* nope, data frame */
+    {
+    }
+    clearInterruptFlag(RFIRQF0, IEEE802154_RFIRQF0_RXPKTDONE);  // Clear package received interrupt flag
+    
+    /** TODO: read RSSI or LQI to global variable */
+  }
+  /* according to (swru191c.pdf) 23.1.2 Interrupt Registers
+     To clear an interrupt from the RF Core, one must clear two flags, both the flag 
+     set in RF Core and the one set in S1CON or TCON (depending on which interrupt
+     is triggered). */
+   S1CON = 0;
+   IEEE802154_ISFLUSHRX();
+}
+
+
+/**
+ * Blocking send of data frame via radio. 
+ * @param: payloadLength: length of frame payload excluding header and CRC
+ * NOTE: only auto-crc is supported right now
+*/
+void IEE802154_radioSentDataFrame(IEE802154_DataFrameHeader_t* header, uint8 payloadLength)
+{
+  uint8 i;
+  
+  /* todo: check length */
+
+  IEEE802154_ISFLUSHTX();          /* Flush TX FIFO */
+
+  clearInterruptFlag(RFIRQF1, IEEE802154_RFIRQF1_TXDONE);   /* Clear TX interrupt */
+  
+  /* write length first. Size of header (without pointer to payload) + 
+     payloadlength + 2 bytes CRC */
+  RFD = (sizeof(IEE802154_DataFrameHeader_t)-1) + payloadLength + IEEE802154_CRCLENGTH;
+  
+  /* now write IEE 802.15.4 header (without pointer to payload of course) */
+  for(i=0;i<sizeof(IEE802154_DataFrameHeader_t)-1 ;i++)
+  {
+    RFD = ((uint8*)header)[i];
+  }
+  
+  /* finally write paylod to buffer */
+  for(i=0;i<payloadLength ;i++)
+  {
+    RFD = header->payload[i];
+  }
+  
+  // Enable TX after calibration
+  IEEE802154_ISTXON();
+  
+  // wait until transmission is finished
+  while ((RFIRQF1 & IEEE802154_RFIRQF1_TXDONE) == 0) ;
+  clearInterruptFlag(RFIRQF1, IEEE802154_RFIRQF1_TXDONE);   // Clear TX interrupt
 }
--- IEEE_802.15.4.h
+++ IEEE_802.15.4.h
@@ -8,13 +8,74 @@
 #include <PlatformTypes.h>
    
 /*******************| Macros |*****************************************/
+/**
+ * interrupt flag bits 
+*/
+#define IEEE802154_RFIRQF0_RXPKTDONE            0x40
+#define IEEE802154_RFIRQF1_TXDONE               0x02
+
+/**
+ * Lenght of CRC in bytes
+*/
+#define IEEE802154_CRCLENGTH                    (uint8)0x02
+   
+/**
+ * Selected strobes for IEEE 802.15.4. See swru191d.pdf Chapter 23.14 Command 
+ * Strobe/CSMA-CA Processor. Quote: "The CSP interfaces with the CPU through the 
+ * SFR register RFST and the XREG registers CSPX, CSPY,CSPZ,CSPT,CSPSTAT,CSPCTRL, 
+ * and CSPPROG < n > (where n is in the range  0 to 23). The CSP produces 
+ * interrupt requests to the CPU. In addition, the CSP interfaces with the MAC 
+ * Timer by observing MAC Timer events."
+ * For an explanation see swru191d.pdf Chapter 25.3.1.2 Command Register
+*/
+#define IEEE802154_ISRXON()                     RFST = 0xE3
+#define IEEE802154_ISTXON()                     RFST = 0xE9
+#define IEEE802154_ISTXONCCA()                  RFST = 0xEA
+#define IEEE802154_ISRFOFF()                    RFST = 0xEF
+#define IEEE802154_ISFLUSHRX()                  RFST = 0xED  /* was EC?? */
+#define IEEE802154_ISFLUSHTX()                  RFST = 0xEE
+
    
 /*******************| Type definitions |*******************************/
+/**
+  * \brief IEEE 802.15.4 frame control field (fcf)
+  * Description of the fields are copied from Contiki project frame802154.h
+  */
+typedef struct {
+  uint16 frameType : 3;               /**< 3 bit. Frame type field, see 802.15.4 */
+  uint16 securityEnabled : 1;         /**< 1 bit. True if security is used in this frame */
+  uint16 framePending : 1;            /**< 1 bit. True if sender has more data to send */
+  uint16 ackRequired : 1;             /**< 1 bit. Is an ack frame required? */
+  uint16 panIdCompression : 1;        /**< 1 bit. Is this a compressed header? */
+  uint16 unused : 3;                  /**< 3 bit. Unused bits (reserved) */
+  uint16 destinationAddressMode : 2;  /**< 2 bit. Destination address mode, see 802.15.4 */
+  uint16 frameVersion : 2;            /**< 2 bit. 802.15.4 frame version */
+  uint16 SourceAddressMode : 2;       /**< 2 bit. Source address mode, see 802.15.4 */
+} IEEE802154_FCF_t;
+
+
+/**
+  * \brief IEEE 802.15.4 frame header according to 802.15.4g-2012 Chapter 5.2.1 General MAC frame format
+  * and 5.2.2.2 Data frame format
+  */
+typedef struct {
+  IEEE802154_FCF_t fcf;
+  uint8 sequenceNumber;
+  uint16 destinationPANID;
+  uint16 destinationAddress;
+#ifndef IEE802154_USE_PANID_COMPRESSION
+  uint16 sourcePANID;
+#endif
+  uint16 sourceAddress;
+  /* todo: add security stuff here */
+  uint8 *payload;                     /**< pointer to payload */
+} IEE802154_DataFrameHeader_t;
 
 /*******************| Global variables |*******************************/
 
 /*******************| Function prototypes |****************************/
 
+void IEE802154_radioSentDataFrame(IEE802154_DataFrameHeader_t* header, uint8 payloadLength);
 #endif
 
 /** @}*/
